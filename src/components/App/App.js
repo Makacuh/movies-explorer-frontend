07a57419d67fb84e8e45c35d533d448ad29afbbe
allvinnik <allvinnik@mail.ru>
2022-09-21 22:01:57 +0300

    ProfileUser,не забыть бургер меню

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,10 @@
 import Main from '../Main/Main';
 import MainMovies from '../MainMovies/MainMovies';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import Login from '../ProfileUser/Login/Login';
+import Profile from '../ProfileUser/Profile/Profile';
+import Register from '../ProfileUser/Register/Register';
+
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
@@ -11,6 +15,9 @@ function App() {
         <Route path='/' element={<Main />} />
         <Route path='/movies' element={<MainMovies />} />
         <Route path='/saved-movies' element={<SavedMovies />} />
+        <Route path='/signin' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signup' element={<Register />} />
 
       </Routes>
     </div>
