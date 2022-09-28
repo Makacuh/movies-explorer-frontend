33f46ea764aca54f97ebda72b36979680bcc0548
allvinnik <allvinnik@mail.ru>
2022-09-28 20:10:59 +0300

    CheckBox&&MoviesCard

--- src/components/MainMovies/Checkbox/Checkbox.js
+++ src/components/MainMovies/Checkbox/Checkbox.js
@@ -1,6 +1,8 @@
+import { useLocation } from 'react-router-dom';
 import './Checkbox.css';
 
-function Checkbox({ onCheckbox, checked, localCheckbox }) {
+function Checkbox({ onCheckbox, checked, checkedSaveMovies }) {
+  const location = useLocation();
   const handleCheckbox = (evt) => {
     onCheckbox(evt.target.checked);
   };
@@ -8,6 +10,7 @@ function Checkbox({ onCheckbox, checked, localCheckbox }) {
   return (
     <div className='checkbox checkbox__container'>
       <p className='checkbox__title'>Короткометражки</p>
+      {location.pathname === '/movies' ? (
       <input
         type='checkbox'
         className='custom-checkbox'
@@ -17,6 +20,17 @@ function Checkbox({ onCheckbox, checked, localCheckbox }) {
         checked={checked}
         onChange={handleCheckbox}
       />
+      ) : (
+        <input
+          type='checkbox'
+          className='custom-checkbox'
+          id='custom-checkbox'
+          name='custom-checkbox'
+          defaultValue='yes'
+          checked={checkedSaveMovies}
+          onChange={handleCheckbox}
+        />
+      )}
       <label htmlFor='custom-checkbox'></label>
     </div>
   );
