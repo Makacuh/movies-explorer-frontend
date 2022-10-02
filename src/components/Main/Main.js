1f855ab22ffcf8f7f8c341a209f306df21882067
allvinnik <allvinnik@mail.ru>
2022-10-02 20:44:29 +0300

    Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import Header from '../Header/Header';
 import LandingHeader from '../Header/LandingHeader/LandingHeader';
+import HeaderMainMovies from '../Header/HeaderMainMovies/HeaderMainMovies';
 import Promo from '../Main/Promo/Promo';
 import NavTab from '../Main/NavTab/NavTab';
 import AboutProject from '../Main/AboutProject/AboutProject';
@@ -8,14 +9,26 @@ import Portfolio from '../Main/Portfolio/Portfolio';
 import AboutStudent from '../Main/AboutStudent/AboutStudent';
 import Footer from '../Footer/Footer';
 
-function Main() {
+function Main({ loggedIn }) {
   return (
-      <><Header
+      <>
+      {loggedIn ? (
+      <Header
+          color={'header__theme_gray'}
+          location={'header__container_movies'}
+        >
+          <HeaderMainMovies />
+        </Header>
+      
+        ) : (
+      <Header
       color={'header__theme_gray'}
       location={'header__box_landing'}
     >
       <LandingHeader />
-    </Header><main className='landing'>
+    </Header>
+    )}
+    <main className='landing'>
         <Promo />
         <NavTab />
         <AboutProject />
